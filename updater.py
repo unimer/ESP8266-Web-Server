8fb2bfb3a9a0df293ca4606121652c4a2330e554
Nikola Marin <marindzoni@gmail.com>
2018-07-07 01:43:15 +0200

    added store, restore and clear

--- updater.py
+++ updater.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from StringIO import StringIO as BytesIO
 
-buff = BytesIO() 
+buff = BytesIO()
 
 def progress(count, total, status=''):
 
@@ -38,22 +38,15 @@ def ftostr(fname):
 	f.close()
 	return new
 
-#def write_data(buffer, size, nmemb, userp):
-	#pass
 
 def sendPost(url, args ):
 	c = pycurl.Curl()
-	
-    #stat = ""
-	#buffer = {'address':address, 'value':value}
-	#print("no")
-	#postfields = urllib.urlencode(args)
 	postfields = urllib.urlencode(args)
 	c.setopt(c.URL, url)
 	c.setopt(c.POSTFIELDS, postfields)
 	c.setopt(c.WRITEFUNCTION, buff.write)
 	c.perform()
-	
+
 	c.close()
 
 
@@ -62,39 +55,42 @@ def getMemory(url):
 	buffer = {'memory':'0'}
 	sendPost(url, buffer)
 
-def servClear(url):
-	buffer = {'clear':'0'}
+def clearEEPROM(url):
+	buffer = {'clear':'1'}
 	sendPost(url, buffer)
 
+def storeToEEPROM(url):
+	buffer = {'store':'1'}
+	sendPost(url, buffer)
 
-def flashMemory(url):
-	buffer = {'flash':'0'}
+def loadFromEEPROM(url):
+	buffer = {'restore':'1'}
 	sendPost(url, buffer)
 
 
 
 def update(fname, url, size):
 
-	data = ftostr(fname)			
+	data = ftostr(fname)
 	address = 0
 	length = len(data)
 	size = int(size)
-	
+
 	buffer = {'update':length}
 	sendPost(url, buffer)
-	
+
 	while address < length:
 		if (address + size) < length:
-			buffer = {'address': address, 'value' : data[address:address + size], 'size': str(size)}			
+			buffer = {'address': address, 'value' : data[address:address + size], 'size': str(size)}
 		else:
 			buffer = {'address': address, 'value' : data[address:(address + (length - address))], 'size': length - address}
 		address += size
 		sendPost(url, buffer)
 		print(buff.getvalue())
-		buff.clear()
-		
-	
-	
+		#buff.clear()
+
+
+
 
 def getUsage():
 	print( "Usage :")
@@ -120,13 +116,15 @@ elif len(sys.argv) == 2:
 			print ( " updater -h will give you informations about right usage")
 	except:
 		pass
+elif len(sys.argv) == 3:
+    if sys.argv[1] == "store":
+    	storeToEEPROM(sys.argv[2]);
+    elif sys.argv[1] == "restore":
+        loadFromEEPROM(sys.argv[2])
+    elif sys.argv[1] == "clear":
+		clearEEPROM(sys.argv[2])
+
 elif len(sys.argv) == 4:
-
-	if sys.argv[1] == "clear":
-		servClear(sys.argv[2])
-
-	elif sys.argv[1] == "flash":
-		flashMemory(sys.argv[2]);
 
 	if not os.path.exists(sys.argv[1]):
 		print(" updater: file " + sys.argv[1] + " does not exist")
